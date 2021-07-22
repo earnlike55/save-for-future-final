2efdc94424be855723b808b37a1160e51758409f
i.jatupornkarnchana <i.jatupornkarnchana@accenture.com>
2021-07-22 14:26:39 +0700

    add login page

--- save-for-future/src/main/java/com/saving/saveforfuture/SaveForFutureApplication.java
+++ save-for-future/src/main/java/com/saving/saveforfuture/SaveForFutureApplication.java
@@ -19,7 +19,7 @@ public class SaveForFutureApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedHeaders("GET","POST","PATCH");
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("GET","POST","PATCH");
 			}
 		};
 	}
