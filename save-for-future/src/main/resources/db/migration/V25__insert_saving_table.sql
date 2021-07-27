c8660db88fc56dd06c15e8d6a4819227ba20eb51
hakyo98 <sunghakyo1998@gmail.com>
2021-07-27 13:44:50 +0700

    change testing customerId from 25 to 26

--- save-for-future/src/main/resources/db/migration/V25__insert_saving_table.sql
+++ save-for-future/src/main/resources/db/migration/V25__insert_saving_table.sql
@@ -1,19 +1,19 @@
 -- Auto-generated SQL script #202107221812
 INSERT INTO saving (savingid,monthlysave,createdatetime,customerid)
-VALUES (1,10000,'2021-07-22',25);
+VALUES (1,10000,'2021-07-22',26);
 INSERT INTO saving (savingid,monthlysave,createdatetime,customerid)
-VALUES (2,14000,'2021-07-30',25);
+VALUES (2,14000,'2021-07-30',26);
 INSERT INTO saving (savingid,monthlysave,createdatetime,customerid)
-VALUES (3,8000,'2021-08-10',25);
+VALUES (3,8000,'2021-08-10',26);
 INSERT INTO saving (savingid,monthlysave,createdatetime,customerid)
-VALUES (4,10000,'2021-08-14',25);
+VALUES (4,10000,'2021-08-14',26);
 INSERT INTO saving (savingid,monthlysave,createdatetime,customerid)
-VALUES (5,10000,'2021-08-20',25);
+VALUES (5,10000,'2021-08-20',26);
 INSERT INTO saving (savingid,monthlysave,createdatetime,customerid)
-VALUES (6,9000,'2021-09-01',25);
+VALUES (6,9000,'2021-09-01',26);
 INSERT INTO saving (savingid,monthlysave,createdatetime,customerid)
-VALUES (7,10000,'2021-09-15',25);
+VALUES (7,10000,'2021-09-15',26);
 INSERT INTO saving (savingid,monthlysave,createdatetime,customerid)
-VALUES (8,11000,'2021-09-24',25);
+VALUES (8,11000,'2021-09-24',26);
 INSERT INTO saving (savingid,monthlysave,createdatetime,customerid)
-VALUES (9,12000,'2021-10-15',25);
+VALUES (9,12000,'2021-10-15',26);
