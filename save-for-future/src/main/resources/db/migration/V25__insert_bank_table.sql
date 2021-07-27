d3884caec07f2f5506e07d1116a6c1350a84cc9d
hakyo98 <sunghakyo1998@gmail.com>
2021-07-27 16:36:59 +0700

    change testing customerId 1000 bankid 3000

--- save-for-future/src/main/resources/db/migration/V25__insert_bank_table.sql
+++ save-for-future/src/main/resources/db/migration/V25__insert_bank_table.sql
@@ -1,2 +1,2 @@
 INSERT INTO bank (bankid,customerid,accounttype,balance,interest,bankname,bankaccno)
-VALUES (3,1,'deposit',40000,0,'Bualuang','7411-334-2234');
+VALUES (3000,1000,'deposit',40000,0,'Bualuang','7411-334-2234');
--- save-for-future/src/main/resources/db/migration/V26__insert_saving_table.sql
+++ save-for-future/src/main/resources/db/migration/V26__insert_saving_table.sql
@@ -1,19 +1,19 @@
 -- Auto-generated SQL script #202107221812
 INSERT INTO saving (savingid,monthlysave,createdatetime,customerid)
-VALUES (1,10000,'2021-07-22',1);
+VALUES (1,10000,'2021-07-22',1000);
 INSERT INTO saving (savingid,monthlysave,createdatetime,customerid)
-VALUES (2,14000,'2021-07-30',1);
+VALUES (2,14000,'2021-07-30',1000);
 INSERT INTO saving (savingid,monthlysave,createdatetime,customerid)
-VALUES (3,8000,'2021-08-10',1);
+VALUES (3,8000,'2021-08-10',1000);
 INSERT INTO saving (savingid,monthlysave,createdatetime,customerid)
-VALUES (4,10000,'2021-08-14',1);
+VALUES (4,10000,'2021-08-14',1000);
 INSERT INTO saving (savingid,monthlysave,createdatetime,customerid)
-VALUES (5,10000,'2021-08-20',1);
+VALUES (5,10000,'2021-08-20',1000);
 INSERT INTO saving (savingid,monthlysave,createdatetime,customerid)
-VALUES (6,9000,'2021-09-01',1);
+VALUES (6,9000,'2021-09-01',1000);
 INSERT INTO saving (savingid,monthlysave,createdatetime,customerid)
-VALUES (7,10000,'2021-09-15',1);
+VALUES (7,10000,'2021-09-15',1000);
 INSERT INTO saving (savingid,monthlysave,createdatetime,customerid)
-VALUES (8,11000,'2021-09-24',1);
+VALUES (8,11000,'2021-09-24',1000);
 INSERT INTO saving (savingid,monthlysave,createdatetime,customerid)
-VALUES (9,12000,'2021-10-15',1);
+VALUES (9,12000,'2021-10-15',1000);
